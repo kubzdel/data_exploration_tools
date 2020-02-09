8c83db3497c2a49580b9a7d2028866b4187d9b70
kubzdel <kubzdel@gmail.com>
2020-02-09 23:27:21 +0100

    No entry.

--- norm_cosine_similarity.py
+++ norm_cosine_similarity.py
@@ -1,11 +1,14 @@
 import numpy as np
 from scipy import stats
 
+#no entry value, outside range
+NO = -1
+
 def process(vec):
     out = []
     for i in vec:
-        if i != -1:
-            out.append(i - stats.tmean(vec, (0, 100))) # rating range
+        if i != NO:
+            out.append(i - stats.tmean(vec, (0, 100)))  # rating range
         else:
             out.append(0)
     return out
@@ -18,10 +21,11 @@ def norm_cos_sim(a, b):
     print(b)
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
-# value outside range for no entry
-a = [4, 3, 4, -1, -1, 1]
-b = [4, 1, 2, 2, -1, 1]
-c = [4, -1, -1, 3, 1, -1]
+
+# NO for no entry
+a = [4, 3, 4, NO, NO, 1]
+b = [4, 1, 2, 2, NO, 1]
+c = [4, NO, NO, 3, 1, NO]
 
 print(norm_cos_sim(a, b))
 print(norm_cos_sim(a, c))
